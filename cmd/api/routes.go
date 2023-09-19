3961e25282b80b2015b8f84b0bfeab4daea735aa
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-19 12:44:25 +0200

    feat: update routes to serve static files

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.notAllowedResponse)
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 	router.HandlerFunc(http.MethodPost, "/services", app.createServiceHandler)
 	router.HandlerFunc(http.MethodGet, "/services/:id", app.showServiceHandler)
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
