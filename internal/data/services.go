e4c612f2d83d90787bfe1953eb233b2a8ff627d9
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-21 14:16:47 +0200

    feat: add validationv to a service

--- internal/data/services.go
+++ internal/data/services.go
@@ -1,9 +1,21 @@
 package data
 
+import "cosmetcab.dp.ua/internal/validator"
+
 type Service struct {
 	ID          int64  `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	URL         string `json:"url"`
 	PhotoURL    string `json:"photo_url"`
+}
+
+func ValidateService(service *Service, v *validator.Validator) {
+	v.Check(service.Title != "", "title", "title must be provided")
+	v.Check(len([]rune(service.Title)) <= 55, "title", "must not be more than 55 chars")
+	v.Check(service.Description != "", "description", "description must be provided")
+	v.Check(len([]rune(service.Description)) >= 20, "description", "description must have more than 20 chars")
+	v.Check(service.URL != "", "url", "url must be provided")
+	v.Check(service.PhotoURL != "", "photo_url", "photo must be provided")
+
 }
