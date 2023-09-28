f717aae183436e8a3bb458a32c9d051ebca5bf63
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-28 10:08:51 +0200

    feat: implement create functionality for categories

--- internal/data/categories.go
+++ internal/data/categories.go
@@ -1,6 +1,10 @@
 package data
 
-import "cosmetcab.dp.ua/internal/validator"
+import (
+	"database/sql"
+
+	"cosmetcab.dp.ua/internal/validator"
+)
 
 type Category struct {
 	ID          int64  `json:"id"`
@@ -14,6 +18,33 @@ func ValidateCategory(category *Category, v *validator.Validator) {
 	v.Check(len([]rune(category.Title)) <= 55, "title", "must not be more than 55 chars")
 	v.Check(category.Description != "", "description", "description must be provided")
 	v.Check(len([]rune(category.Description)) >= 20, "description", "description must have more than 20 chars")
-	v.Check(category.PhotoURL != "", "photo_url", "photo must be provided")
 
+}
+
+type CategoryModel struct {
+	DB *sql.DB
+}
+
+func (c CategoryModel) Insert(category *Category) error {
+	query := `
+	INSERT INTO categories (title, description, photo_url)
+	VALUES ($1, $2, $3)
+	RETURNING id`
+
+	args := []any{category.Title, category.Description, category.PhotoURL}
+
+	return c.DB.QueryRow(query, args...).Scan(&category.ID)
+}
+
+func (c CategoryModel) Get(id int64) (*Category, error) {
+	return nil, nil
+}
+
+func (c CategoryModel) Update(category *Category) error {
+	return nil
+
+}
+
+func (c CategoryModel) Delete(id int64) error {
+	return nil
 }
