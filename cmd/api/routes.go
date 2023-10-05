4cb4487d453dd58add3d3be37a3ce473435acf3e
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-05 13:49:27 +0200

    feat: change method from UPDATE to PATCH

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,7 +15,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 	router.HandlerFunc(http.MethodPost, "/categories", app.createCategoryHandler)
 	router.HandlerFunc(http.MethodGet, "/categories/:id", app.showCategoryHandler)
-	router.HandlerFunc(http.MethodPut, "/categories/:id", app.updateCategoryHandler)
+	router.HandlerFunc(http.MethodPatch, "/categories/:id", app.updateCategoryHandler)
 	router.HandlerFunc(http.MethodDelete, "/categories/:id", app.deleteCategoryHandler)
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 	return app.recoverPanic(app.secureHeaders(router))
