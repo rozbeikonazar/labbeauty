2dab38fbc32ef445528d921728c0d5001428898c
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-08 21:19:24 +0200

    feat: add subcategory model

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,11 +10,13 @@ var (
 )
 
 type Models struct {
-	Categories CategoryModel
+	Categories    CategoryModel
+	SubCategories SubCategoryModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Categories: CategoryModel{DB: db},
+		Categories:    CategoryModel{DB: db},
+		SubCategories: SubCategoryModel{DB: db},
 	}
 }
