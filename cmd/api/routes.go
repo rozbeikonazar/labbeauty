c3fc9c7ea2ee144304b150b7a82b250db55d63e1
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-08 21:20:36 +0200

    feat: add routes for subcategories

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,11 +13,19 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// categories routes
 	router.HandlerFunc(http.MethodGet, "/categories", app.listCategoriesHanlder)
 	router.HandlerFunc(http.MethodPost, "/categories", app.createCategoryHandler)
 	router.HandlerFunc(http.MethodGet, "/categories/:id", app.showCategoryHandler)
 	router.HandlerFunc(http.MethodPatch, "/categories/:id", app.updateCategoryHandler)
 	router.HandlerFunc(http.MethodDelete, "/categories/:id", app.deleteCategoryHandler)
+	// subcategories routes
+	router.HandlerFunc(http.MethodGet, "/subcategories", app.listSubCategoriesHandler)
+	router.HandlerFunc(http.MethodPost, "/subcategories", app.createSubCategoryHandler)
+	router.HandlerFunc(http.MethodGet, "/subcategories/:id", app.showSubCategoryHandler)
+	router.HandlerFunc(http.MethodPut, "/subcategories/:id", app.updateSubCategoryHandler)
+	router.HandlerFunc(http.MethodDelete, "/subcategories/:id", app.deleteSubCategoryHandler)
+
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 	return app.recoverPanic(app.rateLimit(app.secureHeaders(router)))
 }
