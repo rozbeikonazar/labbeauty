cc35d62a5fab5575f664a019697129c922c762ce
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-08 21:17:51 +0200

    feat: add CRUD operations for subcategories table

---
+++ internal/data/subcategories.go
@@ -0,0 +1,95 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"cosmetcab.dp.ua/internal/validator"
+)
+
+type SubCategory struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type SubCategoryModel struct {
+	DB *sql.DB
+}
+
+func ValidateSubCategory(subCategory *SubCategory, v *validator.Validator) {
+	v.Check(subCategory.Name != "", "name", "must be provided")
+	v.Check(len([]rune(subCategory.Name)) >= 8, "name", "name must have more than 8 chars")
+}
+
+func (m SubCategoryModel) Insert(subCategory *SubCategory) error {
+	query := `
+	INSERT INTO subcategories (name)
+	VALUES($1)
+	RETURNING id`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, subCategory.Name).Scan(&subCategory.ID)
+
+}
+
+func (m SubCategoryModel) Get(id int64) (*SubCategory, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	query := `
+	SELECT id, name
+	FROM subcategories
+	WHERE id=$1`
+	var subCategory SubCategory
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(&subCategory.ID, &subCategory.Name)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+
+	}
+	return &subCategory, nil
+}
+
+func (m SubCategoryModel) Update(subCategory *SubCategory) error {
+	query := `
+	UPDATE subcategories
+	SET name=$1
+	WHERE id=$2`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_, err := m.DB.ExecContext(ctx, query, subCategory.Name, subCategory.ID)
+	return err
+
+}
+
+func (m SubCategoryModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	query := `
+	DELETE FROM subcategories
+	WHERE id=$1`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+}
