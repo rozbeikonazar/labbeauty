125daf4761a207dd540d883bebb14163b2d68f12
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-09 14:05:40 +0200

    feat: add service model

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,11 +12,13 @@ var (
 type Models struct {
 	Categories    CategoryModel
 	SubCategories SubCategoryModel
+	Services      ServiceModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Categories:    CategoryModel{DB: db},
 		SubCategories: SubCategoryModel{DB: db},
+		Services:      ServiceModel{DB: db},
 	}
 }
