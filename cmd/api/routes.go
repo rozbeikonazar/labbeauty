5a844bba18cf7738c415ea9e8c3eb4cec6634156
Nazar <root1@Nazars-MacBook-Air.local>
2023-10-09 19:55:04 +0200

    feat: add route for deleteServiceHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -29,6 +29,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/services", app.createServiceHandler)
 	router.HandlerFunc(http.MethodGet, "/services/:id", app.showServiceHandler)
 	router.HandlerFunc(http.MethodPatch, "/services/:id", app.updateServiceHandler)
+	router.HandlerFunc(http.MethodDelete, "/services/:id", app.deleteServiceHandler)
 
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 	return app.recoverPanic(app.rateLimit(app.secureHeaders(router)))
