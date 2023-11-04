dbee22caa1bed75f1496d06840412459e28e1be8
Nazar <root1@Nazars-MacBook-Air.local>
2023-11-04 19:02:46 +0100

    feat: add users table

---
+++ migrations/000004_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id bigserial PRIMARY KEY,
+    name text NOT NULL,
+    email citext UNIQUE NOT NULL, 
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1 
+);
