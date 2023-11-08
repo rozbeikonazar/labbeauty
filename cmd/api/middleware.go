6f94e72c1da3b7b5704bc40abc5798c355bc654c
Nazar <root1@Nazars-MacBook-Air.local>
2023-11-08 20:57:13 +0100

    feat: enable CORS for origin

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -14,6 +14,7 @@ func (app *application) secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+		w.Header().Set("Access-Control-Allow-Origin", "https://cosmetcab.dp.ua/")
 		next.ServeHTTP(w, r)
 
 	})
